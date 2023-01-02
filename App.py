76bb20a606c84aeb230588403a649f10352a400b
khaled166 <khaledseif166@gmail.com>
2023-01-02 21:59:14 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -47,7 +47,7 @@ df = pd.DataFrame({"Gender":[Gender],"CustomerType":[CustomerType],"Age":[Age],"
     
 
 
-#df = pd.get_dummies(df,columns = ["Gender","CustomerType","TypeofTravel","Class"])
+df = pd.get_dummies(df,columns = ["Gender","CustomerType","TypeofTravel","Class"])
 
 '''
 
@@ -62,8 +62,7 @@ for i in range(0,df.shape[1]):
 
 '''
 #Load model 
-load_model = pickle.load(bz2.open("model.pkl","rb"))
-   
+load_model = pickle.load(bz2.open("C:/Users/Khaledseif166/Downloads/All  Of ME/Self-Study-Courses/CDSP_Deploma/Final_Project/Airline_passenger/model.pkl","rb"))
 
 
 #Prediction results based on input data from user.
