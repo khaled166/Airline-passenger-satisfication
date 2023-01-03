3d83567ae69107456c03460781a6d198b3fe8726
khaled166 <khaledseif166@gmail.com>
2023-01-04 00:33:29 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -61,7 +61,7 @@ for i in range(0,df.shape[1]):
 
 '''
 #Load model 
-load_model = pickle.load(bz2.BZ2File(r"C:\Users\Khaledseif166\Downloads\All  Of ME\Self-Study-Courses\CDSP_Deploma\Final_Project\Airline_passenger\model.pkl","rb"))
+load_model = pickle.load(bz2.BZ2File("model.pkl","rb"))
 
 
 #Prediction results based on input data from user.
